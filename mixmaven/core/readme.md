897407b32e30fa3950b3729a15ae12f69080e24c
emsoraffa <emilrso@stud.ntnu.no>
2023-09-21 11:48:04 +0200

    Added readme and codeformatting

--- mixmaven/core/readme.md
+++ mixmaven/core/readme.md
@@ -1,3 +1,5 @@
 # CORE module
 
-This module contains the central classes used in our application.
+This module contains the central classes used in our application. The module is contains three classes and the [json](src/main/java/core/json). The Drink and ingredient classes are data oriented classes and the MixMaven class is used for filehandling, which it achieves by delegating to the relevant utility classes.
+
+The json folder contains a json file and two additional utility classes for writing and reading json. 
--- mixmaven/core/src/main/java/core/Drink.java
+++ mixmaven/core/src/main/java/core/Drink.java
@@ -4,75 +4,76 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Drink implements Serializable{
+public class Drink implements Serializable {
 	private String name;
 	private List<Ingredient> ingredients;
 	private double alcoholContent;
 
-  public Drink(){
-    ingredients = new ArrayList<Ingredient>();
-    alcoholContent = 0;
-  }
-	public Drink(String name){
-		if (validateDrink()){
+	public Drink() {
+		ingredients = new ArrayList<Ingredient>();
+		alcoholContent = 0;
+	}
+
+	public Drink(String name) {
+		if (validateDrink()) {
 			this.name = name;
 			ingredients = new ArrayList<Ingredient>();
 			alcoholContent = 0;
-		} else throw new IllegalArgumentException("Invalid arguments for Drink");
+		} else
+			throw new IllegalArgumentException("Invalid arguments for Drink");
 	}
 
-	public Drink(String name, ArrayList<Ingredient> ingredients){
-		if (validateDrink()){
+	public Drink(String name, ArrayList<Ingredient> ingredients) {
+		if (validateDrink()) {
 			this.name = name;
 			this.ingredients = ingredients;
 			calculateAlcoholContent();
-		} else throw new IllegalArgumentException("Invalid arguments for Drink");
+		} else
+			throw new IllegalArgumentException("Invalid arguments for Drink");
 	}
 
 	public String getName() {
 		return name;
 	}
 
-
 	public List<Ingredient> getIngredients() {
 		return ingredients;
 	}
 
-
 	public double getAlcoholContent() {
 		return alcoholContent;
 	}
-	
-	private boolean validateDrink(){
-		if(alcoholContent<=100) return true;
+
+	private boolean validateDrink() {
+		if (alcoholContent <= 100)
+			return true;
 		return false;
 	}
 
-	public double calculateAlcoholContent(){
+	public double calculateAlcoholContent() {
 		double volume = 0;
 		double alcoholVolume = 0;
 		for (Ingredient ingredient : ingredients) {
-			if (ingredient.getType()=="mixer" || ingredient.getType()=="alcohol"){
-				volume+=ingredient.getAmount();
+			if (ingredient.getType() == "mixer" || ingredient.getType() == "alcohol") {
+				volume += ingredient.getAmount();
 			}
-			alcoholVolume += ingredient.getAlcoholPercentage()*ingredient.getAmount()/100;
-
+			alcoholVolume += ingredient.getAlcoholPercentage() * ingredient.getAmount() / 100;
 
 		}
-		return alcoholVolume/volume;
+		return alcoholVolume / volume;
 	}
 
-	public void addIngredient(Ingredient Ingredient){
+	public void addIngredient(Ingredient Ingredient) {
 		ingredients.add(Ingredient);
 	}
 
-	public void removeIngredient(int index){
+	public void removeIngredient(int index) {
 		ingredients.remove(index);
 	}
-  @Override
-  public String toString() {
-    return "Drink [name=" + name + ", alcoholContent=" + alcoholContent + "]";
-  }
 
-  
+	@Override
+	public String toString() {
+		return "Drink [name=" + name + ", alcoholContent=" + alcoholContent + "]";
+	}
+
 }
