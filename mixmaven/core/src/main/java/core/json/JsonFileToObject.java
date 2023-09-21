9681a6f22d1d6284a1cfa52e406830041a12341e
emsoraffa <emilrso@stud.ntnu.no>
2023-09-21 14:59:39 +0200

    Fixed bug caused by type erasure in JsonFileToObject

--- mixmaven/core/src/main/java/core/json/JsonFileToObject.java
+++ mixmaven/core/src/main/java/core/json/JsonFileToObject.java
@@ -1,17 +1,25 @@
 package core.json;
 
 import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+
+import core.Drink;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-/* Utility class with a method for reading Json and converting it to an Object */
+/* Utility class with a method for reading Json and converting it to a List */
 public class JsonFileToObject {
-	public static Object loadObjectFromJson(String filePath) {
+	public static List<Drink> loadObjectFromJson(String filePath) {
 		Gson gson = new Gson();
 
 		try (FileReader reader = new FileReader(filePath)) {
-			Object obj = gson.fromJson(reader, Object.class);
+
+      Type listType = new TypeToken<List<Drink>>(){}.getType();
+			List<Drink> obj = gson.fromJson(reader, listType);
 
 			reader.close();
 
