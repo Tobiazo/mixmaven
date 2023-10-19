73e051a3e4ab71088a74307ce89125fe9a1a2297
Henriette Nystad <henriesn@stud.ntnu.no>
2023-10-19 10:37:19 +0200

    Added material UI icons

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,5 +1,8 @@
 import data from "../../fixtures/data.json";
 import "../styles/Home.css";
+import LocalBarIcon from '@mui/icons-material/LocalBar';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import EditIcon from '@mui/icons-material/Edit';
 
 const Home = () => {
   return (
@@ -9,13 +12,16 @@ const Home = () => {
         {data.map((drink) => {
           return (
             <li key={drink.id} className="drinkItem">
-              <h2>{drink.name}</h2>
-              <p>{drink.alcoholContent}%</p>
+              <h2> <LocalBarIcon /> {drink.name}</h2>
               <ol>
                 {drink.ingredients.map((ingredient) => (
                   <li className="ingredientItem">{ingredient.name}</li>
                 ))}
               </ol>
+              <p>
+              <DeleteOutlineIcon />
+              <EditIcon />
+              </p>
             </li>
           );
         })}
