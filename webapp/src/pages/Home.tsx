b1f83cb9befb59e4a5934c5e23f66787944b8b87
Isak Berg Endresen <isakben@stud.ntnu.no>
2023-10-19 23:18:29 +0200

    Add animations to homepage with auto-animate

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,8 +1,19 @@
 import DrinkCard from '../components/DrinkCard'
 import data from '../../fixtures/data.json'
 import '../styles/Home.css'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { useState } from 'react'
 
 const Home = () => {
+  const [content, setContent] = useState(data);
+  const [boxRef] = useAutoAnimate<HTMLDivElement>();
+
+  const handleDelete = (index: number) => {
+    const copy = [...content]
+    copy.splice(index, 1)
+    setContent(copy)
+  }
+
   return (
     <>
       <div className="title-box">
@@ -10,9 +21,9 @@ const Home = () => {
         <p>Here is all the drinks from the community</p>
       </div>
 
-      <div className="drink-box">
-        {data.map((drink, index) => (
-          <DrinkCard content={drink} key={'drink_' + index} />
+      <div className="drink-box" ref={boxRef}>
+        {content.map((drink, index) => (
+          <DrinkCard content={drink} key={'drink_' + index} handleDelete={() => handleDelete(index)} />
         ))}
       </div>
     </>
