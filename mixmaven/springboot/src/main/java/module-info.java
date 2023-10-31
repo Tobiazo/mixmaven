b4589631ec327a62205a57e4549810ee99e7bb92
Tobias Osvik Brekke <tobiaobr@stud.ntnu.no>
2023-10-31 10:20:10 +0100

    Setup for springbootserver

---
+++ mixmaven/springboot/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module mixmaven.springboot {
+    requires spring.web;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.core;
+    requires spring.boot.autoconfigure;
+
+    requires mixmaven.core;
+    requires mixmaven.json;
+
+    opens springboot to spring.beans, spring.context, spring.web;
+}
